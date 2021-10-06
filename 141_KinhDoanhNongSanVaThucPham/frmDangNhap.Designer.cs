af399dd78b410cfe6e0c04a9e28404534197ea1f
DangThiThanhLy <lydang323@gmail.com>
2021-10-07 03:13:57 +0700

    first commit

---
+++ 141_KinhDoanhNongSanVaThucPham/frmDangNhap.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace _141_KinhDoanhNongSanVaThucPham
+{
+    partial class frmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDangNhap));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTenDN = new System.Windows.Forms.Label();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.txtTenDN = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.chkHienThiMK = new System.Windows.Forms.CheckBox();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblTenDN, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblMatKhau, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtMatKhau, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.chkHienThiMK, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtTenDN, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnDangNhap, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btnThoat, 2, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(602, 384);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(63, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(475, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTenDN
+            // 
+            this.lblTenDN.AutoSize = true;
+            this.lblTenDN.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTenDN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTenDN.Location = new System.Drawing.Point(63, 115);
+            this.lblTenDN.Margin = new System.Windows.Forms.Padding(3, 0, 3, 7);
+            this.lblTenDN.Name = "lblTenDN";
+            this.lblTenDN.Size = new System.Drawing.Size(174, 50);
+            this.lblTenDN.TabIndex = 1;
+            this.lblTenDN.Text = "Tên đăng nhập";
+            this.lblTenDN.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatKhau.Location = new System.Drawing.Point(63, 172);
+            this.lblMatKhau.Margin = new System.Windows.Forms.Padding(3, 0, 3, 7);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(174, 50);
+            this.lblMatKhau.TabIndex = 2;
+            this.lblMatKhau.Text = "Mật khẩu";
+            this.lblMatKhau.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // txtTenDN
+            // 
+            this.txtTenDN.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtTenDN.Location = new System.Drawing.Point(243, 141);
+            this.txtTenDN.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTenDN.Name = "txtTenDN";
+            this.txtTenDN.Size = new System.Drawing.Size(295, 27);
+            this.txtTenDN.TabIndex = 3;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtMatKhau.Location = new System.Drawing.Point(243, 198);
+            this.txtMatKhau.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.PasswordChar = '*';
+            this.txtMatKhau.Size = new System.Drawing.Size(295, 27);
+            this.txtMatKhau.TabIndex = 4;
+            // 
+            // chkHienThiMK
+            // 
+            this.chkHienThiMK.AutoSize = true;
+            this.chkHienThiMK.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chkHienThiMK.Location = new System.Drawing.Point(243, 233);
+            this.chkHienThiMK.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.chkHienThiMK.Name = "chkHienThiMK";
+            this.chkHienThiMK.Size = new System.Drawing.Size(295, 49);
+            this.chkHienThiMK.TabIndex = 5;
+            this.chkHienThiMK.Text = "Hiển thị mật khẩu ?";
+            this.chkHienThiMK.UseVisualStyleBackColor = true;
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.Image = ((System.Drawing.Image)(resources.GetObject("btnDangNhap.Image")));
+            this.btnDangNhap.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDangNhap.Location = new System.Drawing.Point(63, 289);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(174, 50);
+            this.btnDangNhap.TabIndex = 6;
+            this.btnDangNhap.Text = "Đăng Nhập";
+            this.btnDangNhap.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThoat.Location = new System.Drawing.Point(315, 289);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(150, 50);
+            this.btnThoat.TabIndex = 7;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // frmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 384);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmDangNhap";
+            this.Text = "Đăng nhập hệ thống";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTenDN;
+        private System.Windows.Forms.Label lblMatKhau;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.CheckBox chkHienThiMK;
+        private System.Windows.Forms.TextBox txtTenDN;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
